f3350db000aaafb0884f8260c224653499090110
RenanAlvesEsc <victor.oliveira98@aluno.ce.gov.br>
2022-09-07 17:59:42 -0300

    Adicionei CRUD em django

--- turma/alunos/models.py
+++ turma/alunos/models.py
@@ -5,8 +5,8 @@ import datetime
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=100)
-    cpf = models.BigIntegerField()
-    dtnasc = models.DateField(default=datetime.date.today)
+    cpf = models.CharField(max_length=11)
+    dtnasc = models.DateField(default=datetime.date.today())
     email = models.EmailField()
 
     def __str__(self):
--- turma/alunos/urls.py
+++ turma/alunos/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("cadastro", views.cadastro, name="cadastro")
+    path("cadastro", views.cadastro, name="cadastro"),
+    path("<int:id>/edit", views.edit, name="edit"),
 ]
